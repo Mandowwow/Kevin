c3deb98c047a102cd0c0dce1e34234a07f113315
Setrak Farahian <setoferahian@gmail.com>
2022-04-14 21:02:35 +0200

    basic slash commands added

---
+++ deploy-command.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('serverdate').setDescription('Replies with server creation date'),
+	new SlashCommandBuilder().setName('gay').setDescription('Replies with who is gay'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- index.js
+++ index.js
@@ -10,5 +10,27 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+	else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+	else if (commandName === 'serverdate') {
+		await interaction.reply(`Server created at: ${interaction.guild.createdAt}`);
+	}
+	else if (commandName === 'gay') {
+		await interaction.reply('Adoni is gay.');
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
